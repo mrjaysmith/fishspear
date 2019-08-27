fa81b7ec396f6c71421e3687dff65e6dcc13d475
Jay <kevinkingloveu@gmail.com>
2019-08-27 10:28:17 +0800

    Update FishSpearAdminApplication.java
    update

--- src/main/java/com/fisher/fishspear/FishSpearAdminApplication.java
+++ src/main/java/com/fisher/fishspear/FishSpearAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class FishSpearAdminApplication {
-
+    //springboot程序启动入口
     public static void main(String[] args) {
         SpringApplication.run(FishSpearAdminApplication.class, args);
     }
